054c821abf210e0cb96406a0e1d844b0ef0a0a07
Jamilu Salisu <jamilusalis@gmail.com>
2023-08-19 23:21:29 +0100

    finalize upate

--- wp-content/themes/shoppingcart/footer.php
+++ wp-content/themes/shoppingcart/footer.php
@@ -64,8 +64,8 @@ $footer_column = $shoppingcart_settings['shoppingcart_footer_column_section'];
 	else:
 		echo '<div class="copyright">'; ?>
 		<a title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" target="_blank" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name', 'display' ); ?></a> | 
-						<?php esc_html_e('Designed by:','shoppingcart'); ?> <a title="<?php echo esc_attr__( 'Theme Freesia', 'shoppingcart' ); ?>" target="_blank" href="<?php echo esc_url( 'https://themefreesia.com' ); ?>"><?php esc_html_e('Theme Freesia','shoppingcart');?></a> |
-						<?php  echo '&copy; ' . date_i18n(__('Y','shoppingcart')) ; ?> <a title="<?php echo esc_attr__( 'WordPress', 'shoppingcart' );?>" target="_blank" href="<?php echo esc_url( 'https://wordpress.org' );?>"><?php esc_html_e('WordPress','shoppingcart'); ?></a>
+						<?php esc_html_e('Designed by:','shoppingcart'); ?> <a title="<?php echo esc_attr__( 'PickWise', 'shoppingcart' ); ?>" target="_blank" href="<?php echo esc_url( '#' ); ?>"><?php esc_html_e('PickWise','shoppingcart');?></a> |
+						<?php  echo '&copy; ' . date_i18n(__('Y','shoppingcart')) ; ?>
 					</div>
 	<?php endif; ?>
 			<div style="clear:both;"></div>
